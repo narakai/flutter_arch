018bea39daf630ad6f73202b93becdb85228da1d
laileon <narakai@126.com>
2020-09-29 16:38:13 +0800

    add mask

--- lib/ui/page/tab/expand_page.dart
+++ lib/ui/page/tab/expand_page.dart
@@ -13,6 +13,7 @@ class ExpandPage extends StatefulWidget {
 class _ExpandWidget extends State<ExpandPage> {
   final list = List.generate(10, (index) => "这是第$index个数据");
   final Map<int, bool> map = {};
+  bool _isMaskOffstage = false;
 
   @override
   Widget build(BuildContext context) {
@@ -29,22 +30,58 @@ class _ExpandWidget extends State<ExpandPage> {
         appBar: AppBar(
           title: Text('expansion tile demo'),
         ),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            return CustomExpansionTile(
-              initiallyExpanded: map[index],
-              key: new PageStorageKey(index),
-                // trailing: Container(child: map[index] ? Icon(Icons.map) : Icon(Icons.ac_unit)),
-                title: Container(
-                    child: Text(
-                  "$index个"),
-                ),
-                // onExpansionChanged: (bool expanding) => setState(() => map[index] = expanding),
-                children: list.map((e) => ListTile(title: Text(e))).toList());
-          },
-          itemCount: list.length,
+        body: Stack(
+          children: [
+            ListView.builder(
+              itemBuilder: (context, index) {
+                return CustomExpansionTile(
+                    initiallyExpanded: map[index],
+                    key: new PageStorageKey(index),
+                    // trailing: Container(child: map[index] ? Icon(Icons.map) : Icon(Icons.ac_unit)),
+                    title: Container(
+                      child: Text("$index个"),
+                    ),
+                    // onExpansionChanged: (bool expanding) => setState(() => map[index] = expanding),
+                    children:
+                        list.map((e) => ListTile(title: Text(e))).toList());
+              },
+              itemCount: list.length,
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: _onMaskTap,
+              child: Offstage(
+                offstage: _isMaskOffstage,
+                child: Stack(children: [
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(color: Color(0x90000000)),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 200.0,
+                    decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topLeft: new Radius.circular(0.0),
+                                topRight: new Radius.circular(0.0),
+                                bottomLeft: new Radius.circular(15.0),
+                                bottomRight: new Radius.circular(15.0)))),
+                  )
+                ]),
+              ),
+            )
+          ],
         ),
       ),
     );
+  }
+
+  void _onMaskTap() {
+    setState(() {
+      _isMaskOffstage = !_isMaskOffstage;
+    });
   }
 }
